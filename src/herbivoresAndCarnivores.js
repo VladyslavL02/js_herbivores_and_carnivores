8b0930b1ae0d0e8727d70f5c6ac2c0a5ae18b969
Vladyslav <vlad.lozynsky@gmail.com>
2026-08-14 21:11:13 +0300

    Issue resolved

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -23,7 +23,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (animal.hidden === false) {
+    if (animal.hidden === false && animal instanceof Carnivore) {
       animal.health -= 50;
 
       if (animal.health <= 0) {
